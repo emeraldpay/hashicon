974b76f62d0eccc2434aaf030f895d5b689756c5
Richard Schumann <schumanncombo@gmail.com>
2018-10-16 12:25:58 +0200

    using kecak256 uint16Array, removing chunkHash, applying the new values from this array to the renderer, rewrite the variations to remove the substr() in renderer.

--- src/main.js
+++ src/main.js
@@ -3,18 +3,14 @@ import params from './params';
 import { deepMerge } from './utils';
 import { keccak256 } from 'js-sha3';
 
-
 export default function hashicon(hash, override_params = {}) {
 	
 	// overload "override_params": allow to call with only size value (numeric)
 	let _params = Number.isInteger(override_params) ? { size: override_params } : {...override_params};
 	_params = deepMerge(params, _params);		// Deep merge
 
-	// encrypt hash to keccak256 (64long)
-	var keccak_hash = keccak256(hash);
-	// console.log(keccak_hash);
+	// encrypt hash to a keccak256 Uint16Array.
+	const uint16Array = new Uint16Array(keccak256.arrayBuffer(hash)); 
 
-	return renderer(keccak_hash, _params);
+	return renderer(uint16Array, _params);
 }
-
-
